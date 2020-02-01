802b5b15baf07ab97b8288769f89516929b2ef7e
José Montesinos Navarro <jmontesinos@hotmail.es>
2020-02-01 15:32:28 +0100

    Added user viewset

--- bank_users/users/views.py
+++ bank_users/users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    View set for the user endpoints.
+    """
+    serializer_class = UserSerializer
+    queryset = get_user_model().objects.all()
